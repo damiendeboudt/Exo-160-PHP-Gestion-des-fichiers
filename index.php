a4f75f7b22b4e0e349dd8d1581b2f2881977ae92
Damien <damien.deboudt@sfr.fr>
2023-01-16 15:22:00 +0100

    ok

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
+$lorem = "lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+file_put_contents('montxt.txt', $lorem);
+fopen('montxt.txt', 'rb');
+echo $lorem;
 
-
+echo "<br><br>";
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
  * 4. Faites en sorte d'ajouter chaque chaîne de caractère de ce tableau au fichier créé dans la première partie ( point 2 )
@@ -15,13 +19,31 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$table = [
+    "texte",
+    "new",
+    "renault",
+    "mercedes",
+    "porsche",
+    "citroen",
+    "peugeot",
+    "russe",
+    "ukraine",
+    "vache",
+    "lapin",
+];
+
+echo "<br><br>";
+file_put_contents('montxt.txt', $table, FILE_APPEND);
 
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+$file = '/montxt.txt';
+$info = pathinfo($file);
+echo "extension au format ." . $info['extension'] . "<br>";
 
 
 /**
@@ -29,7 +51,11 @@
  */
 // TODO Votre code ici.
 
-
+if(file_exists('toto')) {
+    echo 'ce fichier existe';
+} else {
+     echo "ce fichier n'existe pas";
+}
 /**
  * Super bonus.
  * Parcourrez votre fichier, à chaque fois que vous rencontrez le caractère 'a', remplacez le par le caractère '@'
